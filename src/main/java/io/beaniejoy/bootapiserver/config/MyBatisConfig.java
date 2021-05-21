be151f2ac95db919c6d282c2aa07877d6748e0ed
beaniejoy <lhb1025@gmail.com>
2021-05-22 02:37:28 +0900

    [#3] MyBatis Config 관련
    - set Config java file
    - set xml file

--- src/main/java/io/beaniejoy/bootapiserver/config/MyBatisConfig.java
+++ src/main/java/io/beaniejoy/bootapiserver/config/MyBatisConfig.java
@@ -4,8 +4,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -15,6 +17,9 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan(basePackages = "io.beaniejoy.bootapiserver.mapper")
 public class MyBatisConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -29,6 +34,10 @@ public class MyBatisConfig {
         sqlSessionFactoryBean.setDataSource(dataSource);
         sqlSessionFactoryBean.setMapperLocations(
                 new PathMatchingResourcePatternResolver().getResources("classpath:/mappers/**/*.xml")
+        );
+        sqlSessionFactoryBean.setTypeAliasesPackage("io.beaniejoy.bootapiserver.model");
+        sqlSessionFactoryBean.setConfigLocation(
+                applicationContext.getResource("classpath:/mybatis-config.xml")
         );
 
         return sqlSessionFactoryBean.getObject();
